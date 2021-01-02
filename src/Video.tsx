8057057837caa481afd28c36ee14bd9fbcf23066
Jonny Burger <jonathanburger11@gmail.com>
2021-01-02 15:24:04 +0100

    update template

--- src/Video.tsx
+++ src/Video.tsx
@@ -13,7 +13,7 @@ export const RemotionVideo: React.FC = () => {
 				fps={30}
 				width={1920}
 				height={1080}
-				props={{
+				defaultProps={{
 					titleText: 'Welcome to Remotion',
 					titleColor: 'black',
 				}}
