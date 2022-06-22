f3f553c3d1e21759096abd16d705524f05b4a46c
Jonny Burger <jonathanburger11@gmail.com>
2022-06-22 19:15:17 +0200

    elaborate more

--- src/Video.tsx
+++ src/Video.tsx
@@ -3,26 +3,27 @@ import {HelloWorld} from './HelloWorld';
 import {Logo} from './HelloWorld/Logo';
 
 // Each <Composition> is an entry in the sidebar!
-// You can take it's "id" to render a video:
-// npx remotion render src/index.tsx <composition-id> out/video.mp4
 
 export const RemotionVideo: React.FC = () => {
 	return (
 		<>
 			<Composition
+				// You can take the "id" to render a video:
+				// npx remotion render src/index.tsx <id> out/video.mp4
 				id="HelloWorld"
 				component={HelloWorld}
 				durationInFrames={150}
 				fps={30}
 				width={1920}
 				height={1080}
-				// You can override these props at render:
+				// You can override these props for each render:
 				// https://www.remotion.dev/docs/parametrized-rendering
 				defaultProps={{
 					titleText: 'Welcome to Remotion',
 					titleColor: 'black',
 				}}
 			/>
+			{/* Mount any React component to make it show up in the sidebar and work on it individually! */}
 			<Composition
 				id="OnlyLogo"
 				component={Logo}
