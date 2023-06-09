d24df8307453a32f761daacf72fd05f38b27b335
Patric Salvisberg <89481679+patsalv@users.noreply.github.com>
2023-06-09 13:30:55 +0200

    updated comment in config

--- remotion.config.ts
+++ remotion.config.ts
@@ -1,7 +1,8 @@
 // See all configuration options: https://remotion.dev/docs/config
 // Each option also is available as a CLI flag: https://remotion.dev/docs/cli
 
-// Note: The configuration file does only apply if you render via the CLI !
+// ! The configuration file does not apply if you use the Node.js API
+
 
 import {Config} from 'remotion';
 
